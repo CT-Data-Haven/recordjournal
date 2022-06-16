d7632269e7f57de0900ca05cf4e56ed8d5664779
davilak <46454450+davilak@users.noreply.github.com>
2022-06-16 17:52:58 -0400

    remove emails 2

--- survey_data/remove_emails.R
+++ survey_data/remove_emails.R
@@ -1,21 +1,20 @@
 #remove email addresses but keep the field
 library(tidyverse)
 path <- "survey_data/"
-date <- "05162022/"
+date <- "06132022/"
 eng <- "(English) RJ Latino Communities Reporting Lab Survey (Responses) - Form Responses 1.csv"
 spn <- "(Spanish) RJ Latino Communities Reporting Lab Survey (Responses) - Form Responses 1.csv"
 
 read_eng <- read_csv(file = paste(path, date, eng, sep = ""))
 
 write_eng <- read_eng %>%
-  mutate(`If you'd like to receive a digital coupon to CTown Supermarket for completing the survey (and be entered to win a CTown gift card) please provide your email address below.` = if_else(grepl("@", `If you'd like to receive a digital coupon to CTown Supermarket for completing the survey (and be entered to win a CTown gift card) please provide your email address below.`), "REMOVED", "NOT PROVIDED"))
-
+  mutate(`If you'd like to receive a digital coupon to CTown Supermarket for completing the survey, please provide your email address below. The coupon is valid for $5 off of a purchase of $50 or more OR $10 off of a purchase of $100 or more.` = if_else(grepl("@", `If you'd like to receive a digital coupon to CTown Supermarket for completing the survey, please provide your email address below. The coupon is valid for $5 off of a purchase of $50 or more OR $10 off of a purchase of $100 or more.`), "REMOVED", "NOT PROVIDED"))
 
 write_csv(write_eng, file = paste(path, date, eng, sep = ""))
 
 read_spn <- read_csv(file = paste(path, date, spn, sep = ""))
 
 write_spn <- read_spn %>%
-  mutate(`Si desea recibir un cupón digital de CTown Supermarket, por completar la encuesta, y también participar para ganar una tarjeta de regalo de CTown, por favor ingrese su dirección de correo electrónico a continuación.` = if_else(grepl("@", `Si desea recibir un cupón digital de CTown Supermarket, por completar la encuesta, y también participar para ganar una tarjeta de regalo de CTown, por favor ingrese su dirección de correo electrónico a continuación.`), "REMOVED", "NOT PROVIDED"))
+  mutate(`Si desea recibir un cupón digital de CTown Supermarket, por completar la encuesta, por favor ingrese su dirección de correo electrónico a continuación. Este cupón es válido para $5 de descuento en compras de $50 o más o $10 de descuento en compras de $100 o más.`  = if_else(grepl("@", `Si desea recibir un cupón digital de CTown Supermarket, por completar la encuesta, por favor ingrese su dirección de correo electrónico a continuación. Este cupón es válido para $5 de descuento en compras de $50 o más o $10 de descuento en compras de $100 o más.`), "REMOVED", "NOT PROVIDED"))
 
 write_csv(write_spn, file = paste(path, date, spn, sep = ""))
